628100f86a38021eb16dc76aec986dfb78cfcb8a
ThaiLe <levohuuthai1@gmail.com>
2021-09-14 23:21:04 +0700

    branch native

--- src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
+++ src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
@@ -2,7 +2,20 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    @Query(
+            value = "select * from employee",
+            nativeQuery = true
+    )
+    List<Employee> selectNative();
 
+    @Query(
+            value = "delete * from employee",
+            nativeQuery = true
+    )
+    boolean deleteNative();
 }
