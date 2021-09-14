d20b98bb137baf7647277c9ba5a79adbb364efe8
ThaiLe <levohuuthai1@gmail.com>
2021-09-14 23:27:47 +0700

    branch jpql

--- src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
+++ src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+    @Query("from Employee")
+    List<Employee> selectEmployeeInJPQL();
 }
--- src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,11 +1,19 @@
 package com.example.springdatajpa;
 
+import com.example.springdatajpa.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    @Test
+    void testSelect() {
+        System.out.println(employeeRepository.selectEmployeeInJPQL());
+    }
     @Test
     void contextLoads() {
     }
