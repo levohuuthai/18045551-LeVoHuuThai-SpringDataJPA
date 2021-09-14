70455a21cd15ec214abb89e7328ac83d70472bbe
ThaiLe <levohuuthai1@gmail.com>
2021-09-14 23:31:41 +0700

    branch native

--- src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,11 +1,21 @@
 package com.example.springdatajpa;
 
+import com.example.springdatajpa.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    //Test on native branch
+
+    @Test
+    void testSelect() {
+        System.out.println(employeeRepository.selectNative());
+    }
     @Test
     void contextLoads() {
     }
