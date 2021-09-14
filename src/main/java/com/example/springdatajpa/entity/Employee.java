849ab42a23a5eee6faa0411a84e37bff64b04865
ThaiLe <levohuuthai1@gmail.com>
2021-09-14 22:26:43 +0700

    branch main

---
+++ src/main/java/com/example/springdatajpa/entity/Employee.java
@@ -0,0 +1,21 @@
+package com.example.springdatajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/example/springdatajpa/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.springdatajpa.repository;
+
+import com.example.springdatajpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
